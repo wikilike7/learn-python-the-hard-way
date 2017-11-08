761fb7a49dcf0c8b84f0361d8d97c4aef30e4750
wikilike7 <wikilike7@gmail.com>
2017-11-08 18:01:36 +0800

    add lesson-26,28,30

---
+++ lesson-28.py
@@ -0,0 +1,24 @@
+# 下面是练习的答案
+1. True
+2. False
+3. False
+4. True
+5. True
+6. True
+7. False
+8. True
+9. False
+10. False
+11. True
+12. False
+13. True
+14. False
+15. False
+16. False
+17. True
+18. False
+19. False
+20. False
+
+# 符号
+> >= < <= != + - * / %
---
+++ lesson-29.py
@@ -0,0 +1,20 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("Too many cats! The world is doomed!")
+elif people > cats:
+    print("Not many cats! The world is saved!")
+elif people < dogs:
+    print('The world is drooled on!')
+elif people > dogs:
+    print('The world is dry!')
+
+dogs += 5
+if people >= dogs:
+    print('People are greater than or equal to dogs.')
+elif people <= dogs:
+    print('People are less than equal to dogs.')
+elif people == dogs:
+    print('People are dogs.')
