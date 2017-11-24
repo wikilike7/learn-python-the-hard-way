8a0044535ab3ea80daed8b6b377015db5871e4e4
wikilike7 <wikilike7@gmail.com>
2017-11-24 09:47:51 +0800

    add 40.py & mystuff.py

--- lesson-40.py
+++ lesson-40.py
@@ -4,3 +4,42 @@ class Mystuff(object):
 
     def apple(self):
         print('I AM CLASSY APPLES!')
+
+
+thing = Mystuff()
+thing.apple()
+print(thing.tangerine)
+
+
+class Song:
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+
+happy_bday = Song(['Happy birthday to you',
+                   'I don\'t want to get sued',
+                   'So i\'ll stop right there'])
+
+bulls_on_parade = Song(['They rally around the family',
+                       'With pockets full of shells'])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
+
+
+class City(object):
+    def __init__(self, people, cars):
+        self.people = people
+        self.cars = cars
+
+    def print_people_cars(self):
+        print('In city we have %s people and %s cars' % (self.people, self.cars))
+
+
+result = City(1000, 200)
+result.print_people_cars()
--- mystuff.py
+++ mystuff.py
@@ -6,10 +6,3 @@ def apple():
 # this is just a variable
 tangerine = 'Living reflection of a dream'
 
-my_list = [
-    1, 2, 3,
-    4, 5, 6,
-]
-
-for i in my_list:
-    print(i)
