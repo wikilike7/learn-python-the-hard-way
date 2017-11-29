bde4a7daaa9e5921424c980aafa1b788a6a4a710
wikilike7 <wikilike7@gmail.com>
2017-11-29 15:16:00 +0800

    add '访问限制.py'

---
+++ 访问限制.py
@@ -0,0 +1,23 @@
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def print_score(self):
+        print('%s: %s' % (self.__name, self.__score))
+
+    def get_name(self):
+        return self.__name
+
+    def get_score(self):
+        return self.__score
+
+    def set_score(self, score):
+        if 0 <= score <= 100:
+            self.score = score
+        else:
+            raise ValueError('bad score')
+
+
+bart = Student('Bart Simpson', 59)
+
