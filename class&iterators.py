7b3c421eb79463efa2ad91d0e7c4a3b435841b29
wikilike7 <wikilike7@gmail.com>
2017-11-29 18:03:18 +0800

    add class & iteration

---
+++ class&iterators.py
@@ -0,0 +1,28 @@
+# class Fib:
+#     def __init__(self, max):
+#         self.max = max
+#
+#     def __iter__(self):
+#         self.a = 0
+#         self.b = 1
+#         return self
+#
+#     def __next__(self):
+#         fib = self.a
+#         if fib > self.max:
+#             raise StopIteration
+#         self.a, self.b = self.b, self.a + self.b
+#         return fib
+
+
+# define a class
+class PapayaWhip:
+    pass  # pass is like a placeholder
+
+
+class Fib:
+    def __init__(self, max):
+
+
+
+
